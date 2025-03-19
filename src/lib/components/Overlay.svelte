33dc11571ef4c1810bb3d389ca318e959375eef3
Al Zaplin <zaplin.dev@gmail.com>
2025-03-19 08:39:41 +0300

    fix (modal window) Added a script that checks if there is a scrollbar on the page and adds its width to prevent content from jumping to the right when overflow: hidden style is enabled.

--- src/lib/components/Overlay.svelte
+++ src/lib/components/Overlay.svelte
@@ -1,19 +1,25 @@
-<!-- <script lang="ts">
-	const getScrollbarWidth = () =>
-		console.log(window.innerWidth - document.documentElement.clientWidth);
+<script lang="ts">
+	let scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
 
-	getScrollbarWidth();
-</script> -->
+	const hasYScroll = () => {
+		if (scrollbarWidth > 0) return true;
+	};
+</script>
 
 <!-- When we write so it will apply but everywhere and in any time. So we have to make it differently and put it inside "svelte:head" tag, so it applies only when Overlay is active on the page. -->
 <svelte:head>
-	<style>
-		body {
-			overflow: hidden;
-			padding-right: 17px;
-			background-color: var(--color-whisper);
-		}
-	</style>
+	{#if hasYScroll()}
+		<style>
+			/* :global(:root) {
+					--scrollbarWidth: {scrollbarWidth}px;
+				} */
+			body {
+				overflow: hidden;
+				padding-right: 17px;
+				background-color: var(--color-whisper);
+			}
+		</style>
+	{/if}
 </svelte:head>
 
 <!-- Actually it’s better to abstract this and make an extra component Overlay, that will wraps modal window. -->
