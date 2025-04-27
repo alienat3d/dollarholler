ad7099f9f2067a5e3335fb47060ec55de70c6616
Al Zaplin <zaplin.dev@gmail.com>
2025-04-27 17:09:02 +0300

    Add(code & styles) Lesson #30 'Adding toggle button for a new client and additional fields to it, also select US states, which takes all the names from states array'

--- src/routes/(dashboard)/invoices/InvoiceForm.svelte
+++ src/routes/(dashboard)/invoices/InvoiceForm.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+  import { slide } from 'svelte/transition';
   import { v4 as uuidv4 } from 'uuid';
   import LineItemRows from './LineItemRows.svelte';
   import Button from '$lib/components/Button.svelte';
   import Trash from '$lib/components/Icon/Trash.svelte';
+  import { states } from '$lib/utils/states';
 
   const blankLineItem = {
     id: uuidv4(),
@@ -12,6 +14,7 @@
   };
 
   let lineItems: LineItem[] = [{ ...blankLineItem }];
+  let isNewClient: boolean = false;
 
   const AddLineItem = () => {
     lineItems = [...lineItems, { ...blankLineItem, id: uuidv4() }];
@@ -31,7 +34,8 @@
 
 <form class="grid grid-cols-6 gap-x-5">
   <!-- client -->
-  <div class="field col-span-2">
+  <!-- * 30.0 Поработаем над тем, чтобы у нас помимо кнопки добавления нового клиенты была также кнопка добавления уже существующего и появляться она будет по условию, если у нас isNewClient в положении true. Т.е. у пользователя будет выбор либо воспользоваться теми клиентами, что у него уже сохранены, либо добавить нового. Мы объединим эти две колонки в одну, а ещё нам нужно, чтобы селект и кнопка были на одной линии.  -->
+  <!-- <div class="field col-span-2">
     <label for="client">Client</label>
     <select name="client" id="client">
       <option value="zeal">ZEAL</option>
@@ -40,6 +44,40 @@
   <div class="field col-span-2 flex items-end gap-x-5">
     <div class="text-base font-bold leading-[3.5rem] text-monsoon">or</div>
     <Button label="+ Client" onClick={() => {}} style="outline" isAnimated={false} />
+  </div> -->
+  <div class="field col-span-4">
+    {#if !isNewClient}
+      <label for="client">Client</label>
+      <div class="flex items-end gap-x-5">
+        <select name="client" id="client">
+          <option value="zeal">ZEAL</option>
+        </select>
+        <div class="text-base font-bold leading-[3.5rem] text-monsoon">or</div>
+        <Button
+          label="+ Client"
+          onClick={() => {
+            isNewClient = true;
+          }}
+          style="outline"
+          isAnimated={false}
+        />
+      </div>
+    {:else}
+      <!-- 30.1 Ну и для нового клиентам нам понадобятся некоторые дополнительные поля: электронная почта и обычная почта. -->
+      <label for="newClient">New Client</label>
+      <div class="flex items-end gap-x-5">
+        <input type="text" name="newClient" />
+        <div class="text-base font-bold leading-[3.5rem] text-monsoon">or</div>
+        <Button
+          label="Existing Client"
+          onClick={() => {
+            isNewClient = false;
+          }}
+          style="outline"
+          isAnimated={false}
+        />
+      </div>
+    {/if}
   </div>
 
   <!-- invoice id -->
@@ -47,6 +85,44 @@
     <label for="id">Invoice ID</label>
     <input type="number" name="id" />
   </div>
+
+  <!-- new client -->
+  <!-- 30.2 Добавим также анимацию с помощью атрибута transition. -->
+  <!-- Go to [src\lib\utils\states.ts] -->
+  {#if isNewClient}
+    <div class="field col-span-6 grid gap-x-5" transition:slide>
+      <div class="field col-span-6">
+        <label for="email"> Client's email </label>
+        <input type="email" name="email" id="email" />
+      </div>
+
+      <div class="field col-span-6">
+        <label for="street">Street</label>
+        <input type="text" name="street" id="street" />
+      </div>
+
+      <div class="field col-span-2">
+        <label for="city">City</label>
+        <input type="text" name="city" id="city" />
+      </div>
+
+      <!-- 30.4 Используем наш массив со штатами США здесь. -->
+      <div class="field col-span-2">
+        <label for="state">State</label>
+        <select name="state" id="state">
+          <option />
+          {#each states as state}
+            <option value={state.value}>{state.name}</option>
+          {/each}
+        </select>
+      </div>
+
+      <div class="field col-span-2">
+        <label for="zip">Zip</label>
+        <input type="text" name="zip" id="zip" />
+      </div>
+    </div>
+  {/if}
 
   <!-- due date -->
   <div class="field col-span-2">
