4b7279f013cf86c7b03b6d33e6e110afabb2ca1c
Al Zaplin <zaplin.dev@gmail.com>
2025-05-04 08:58:30 +0300

    add (styles) Transition animation for SlidePanel comp. and it's Overlay comp. too

--- comments/lib/components/Overlay (63).svelte
+++ comments/lib/components/Overlay (63).svelte
@@ -13,6 +13,7 @@
   </style>
 </svelte:head>
 
+<!-- 63.1 Всё выглядит здорово за исключением подкладки, которая включается и выключается всё ещё резко. Исправим это. И тут нам уже подойдёт другая анимация угасания\появления от Svelte "fade". -->
 <div
   class={`fixed inset-0 z-overlay bg-blueGem opacity-60 ${className}`}
   transition:fade={{ duration: 500, easing: elasticInOut }}
--- comments/lib/components/SlidePanel (63).svelte
+++ comments/lib/components/SlidePanel (63).svelte
@@ -18,6 +18,8 @@
 
 <Portal>
   <Overlay />
+  <!-- * 63.0 Занимаемся анимацией компонентов дальше. Теперь пришёл черёд выдвижной панели с формами. Здесь, в отличии от строки поиска мы уже используем арсенал самого Svelte и в частности анимацию движения "fly". И нам нужно, чтобы панель выдвигалась справа, т.к. добавим также нужные настройки. -->
+  <!-- Go to [src\lib\components\Overlay.svelte] -->
   <div
     class="fixed right-0 top-0 z-slidePanel h-screen w-full overflow-y-scroll bg-white px-5 pt-16    shadow-slidePanel lg:w-3/4 lg:py-20 lg:px-32"
     transition:fly={{ x: 1000, duration: 500 }}
