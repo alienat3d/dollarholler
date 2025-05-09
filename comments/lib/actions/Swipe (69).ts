3ca75a30a333c3976ece4c8799d04acfd7b3a743
Al Zaplin <zaplin.dev@gmail.com>
2025-05-09 18:01:34 +0300

    Copy & clean comments for the lesson

--- comments/lib/actions/Swipe (69).ts
+++ comments/lib/actions/Swipe (69).ts
@@ -15,6 +15,7 @@ export const swipe: Action<HTMLElement, SwipeProps> = (node, params) => {
 
   coordinates.subscribe(($coords) => (node.style.transform = `translate3d(${$coords.x}px, 0, 0)`));
 
+  // * 69.0 На самом деле нам нужно подключать функцию свайпа лишь от определённой ширины вьюпорта, т.к. на десктопных экранах пользователи пользуются мышкой соответственно. Для проверки этого мы создадим спец. функцию "isMobileBreakpoint", которая будет проверять вьюпорт пользовательского устройства. Поместим результат выполнения спец. метода браузерного API "matchMedia" в константу "mediaQuery" и в этот метод поместим максимальную ширину вьюпорта, от которого мы хотим сделать, чтобы работал свайп экшен. Затем напишем условие, что если mediaQuery "matches", т.е. true — то и возвращать будем true. ↓
   const isMobileBreakpoint = () => {
     const mediaQuery = window.matchMedia('(max-width: 1024px)');
 
@@ -83,8 +84,11 @@ export const swipe: Action<HTMLElement, SwipeProps> = (node, params) => {
     window.removeEventListener('mouseup', handleMouseUp);
   };
 
+  // 69.1 А дальше мы можем обернуть наш слушатель в проверку новой функцией проверки вьюпорта.
   if (isMobileBreakpoint()) node.addEventListener('mousedown', handleMouseDown);
 
+  // 69.2 Если же мы хотим, чтобы не только при первом заходе происходила проверка ширины вьюпорта, но и при изменении ширины браузера — нам требуется написать отдельный слушатель для этого.
+  // 69.3 Теперь, раз уж мы сделали слушатель события "resize", то можно также починить этот баг, чтобы инвойсы не уезжали дальше поля видимости, если продолжать сужать экран. А сделать это довольно просто, присваивая elementWidth новое значение при ресайзе экрана браузера.
   window.addEventListener('resize', () => {
     isMobileBreakpoint()
       ? node.addEventListener('mousedown', handleMouseDown)
