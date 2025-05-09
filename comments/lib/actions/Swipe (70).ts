419c7bb17555f027249f6643606bb55c9ee942f2
Al Zaplin <zaplin.dev@gmail.com>
2025-05-09 22:13:55 +0300

    feat (touch events) For "Swipe" action; + Some refactoring.

--- comments/lib/actions/Swipe (70).ts
+++ comments/lib/actions/Swipe (70).ts
@@ -21,6 +21,8 @@ export const swipe: Action<HTMLElement, SwipeProps> = (node, params) => {
     if (mediaQuery.matches) return true;
   };
 
+  // 70.0.1 А также она должна вызваться как только этот код считается ↓
+  // 70.1 Теперь мы добавим к событиям мышки также тач-события. ↓
   const setupEventListeners = () => {
     if (isMobileBreakpoint()) {
       node.addEventListener('mousedown', handleMouseDown);
@@ -51,6 +53,7 @@ export const swipe: Action<HTMLElement, SwipeProps> = (node, params) => {
     window.addEventListener('mouseup', handleMouseUp);
   };
 
+  // 70.2 Здесь функция для начала движения на тачскрине будет похожа на предыдущую "handleMouseDown", за искл., что после "event" добавляется "touches[0]", ну и события будут "touchmove" & "touchend".
   const handleTouchStart = (event: TouchEvent) => {
     x = event.touches[0].clientX;
     startingX = event.touches[0].clientX;
@@ -59,6 +62,7 @@ export const swipe: Action<HTMLElement, SwipeProps> = (node, params) => {
     window.addEventListener('touchend', handleTouchEnd);
   };
 
+  // 70.3 Также и "handleTouchMove" будет очень похожа на "handleMouseMove" за некоторыми корректировками.
   const handleMouseMove = (event: MouseEvent) => {
     const dx = event.clientX - x;
 
@@ -75,6 +79,7 @@ export const swipe: Action<HTMLElement, SwipeProps> = (node, params) => {
     updateCoordinates(dx);
   };
 
+  // 70.4 Сделаем и тут небольшой рефакторинг: переименуем в "setXCoordinates", потому, как это название более соответствует тому, что делает эта функция, а затем создадим ещё одну функцию "updateCoordinates" и перенесём туда код из "handleTouchMove", т.к. нам нужно будет его переиспользовать снова.
   const updateCoordinates = (dx: number) => {
     coordinates.update(($coords) => {
       return {
@@ -115,6 +120,7 @@ export const swipe: Action<HTMLElement, SwipeProps> = (node, params) => {
     window.removeEventListener('mouseup', handleMouseUp);
   };
 
+  // 70.5 Осталось создать "handleTouchEnd", которая также будет похожа на "handleMouseUp". Однако вместо "touches[0]" как в предыдущем примере мы используем "changedTouches[0]", которая хранит разницу изменения позиции с начальной до конечной. ↓
   const handleTouchEnd = (event: TouchEvent) => {
     const endingX = event.changedTouches[0].clientX;
     moveCardOver(endingX);
@@ -123,10 +129,16 @@ export const swipe: Action<HTMLElement, SwipeProps> = (node, params) => {
     window.removeEventListener('touchend', handleTouchEnd);
   };
 
+  // 70.0.3 И теперь можно избавиться от этого условия, т.к. оно уже включено в функцию "setupEventListeners". ↑
+  // if (isMobileBreakpoint()) node.addEventListener('mousedown', handleMouseDown);
+
+  // * 70.0.0 В этом уроке мы переведём этот экшен (точнее его слушатели событий) с событий мышки на события тапа (прикосновения пальца к экрану). Но прежде займёмся небольшим рефакторингом и вынесем логику из этого слушателя в отдельную функцию "setupEventListeners", которую создадим выше. ↑
   window.addEventListener('resize', setupEventListeners);
 
+  // 70.0.2
   setupEventListeners();
 
+  // 70.6 Также нужно удалять слушатель для "touchstart" по окончанию события, чтобы они не множились.
   return {
     update(newParams: SwipeProps) {
       if (newParams.triggerReset) resetCard();
